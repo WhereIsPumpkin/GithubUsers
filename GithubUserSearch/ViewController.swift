7d8462281bdd5f4653f0a0a56f0bb747eded2085
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2023-12-29 23:34:48 +0400

    initial commit

---
+++ GithubUserSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GithubUserSearch
+//
+//  Created by Saba Gogrichiani on 29.12.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+
+}
+
