d844e7d730ff25a8dbdc795f699adacddd3fa918
Saba Gogrichiani <sgogr2021@agruni.edu.ge>
2023-12-30 00:05:37 +0400

    feat: add tabbar

---
+++ GithubUserSearch/FavoritesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  GithubUserSearch
+//
+//  Created by Saba Gogrichiani on 29.12.23.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+}
--- GithubUserSearch/SearchVC.swift
+++ GithubUserSearch/SearchVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchVC.swift
 //  GithubUserSearch
 //
 //  Created by Saba Gogrichiani on 29.12.23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        
+        view.backgroundColor = .systemRed
     }
-
-
 }
-
